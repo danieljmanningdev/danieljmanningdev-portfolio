43118a9c08f50e25114a1d506367ce0231db90d6
SympticDevelopment <developer@symptic.co.uk>
2026-08-09 23:12:03 +0100

    feat: add project form validation

--- internal/http/project_data.go
+++ internal/http/project_data.go
@@ -1,1 +1,22 @@
 package http
+
+import (
+	"github.com/danieljmanningdev/danieljmanningdev-portfolio/internal/models"
+)
+
+type projectsPageData struct {
+	Title    string
+	Projects []models.Project
+}
+
+type projectPageData struct {
+	Title   string
+	Project models.Project
+}
+
+type projectFormPageData struct {
+	Title   string
+	Form    projectForm
+	Errors  projectFormErrors
+	Clients []models.Client
+}
