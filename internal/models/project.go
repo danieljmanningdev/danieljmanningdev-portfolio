2cfec6a56debe99f28ab9cc5de29a8a1a2f93822
SympticDevelopment <developer@symptic.co.uk>
2026-08-09 21:16:37 +0100

    chore: scaffold projects domain

---
+++ internal/models/project.go
@@ -0,0 +1,16 @@
+package models
+
+import "time"
+
+type Project struct {
+	ID          int64
+	ClientID    int64
+	ClientName  string
+	Name        string
+	Description string
+	Status      string
+	StartDate   *time.Time
+	DueDate     *time.Time
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
