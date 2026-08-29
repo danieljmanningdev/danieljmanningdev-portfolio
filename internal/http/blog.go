accc094d043684238eaa21cb3dcdbc87eb908e1b
danieljmanningdev <daniel@danieljmanningdev.com>
2026-08-29 14:25:59 +0100

    feat: add blog handler logic

--- internal/http/blog.go
+++ internal/http/blog.go
@@ -1,1 +1,45 @@
 package http
+
+import (
+	"database/sql"
+	"html/template"
+
+	"github.com/danieljmanningdev/danieljmanningdev-portfolio/internal/rendering"
+	"github.com/danieljmanningdev/danieljmanningdev-portfolio/internal/repository"
+)
+
+type BlogHandler struct {
+	repository *repository.BlogRepository
+
+	blogTemplates     *template.Template
+	blogPostTemplates *template.Template
+}
+
+func NewBlogHandler(
+	db *sql.DB,
+	templateDir string,
+) (*BlogHandler, error) {
+	repo := repository.NewBlogRepository(db)
+
+	blogTemplates, err := rendering.LoadPageTemplate(
+		templateDir,
+		"blog.html",
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	blogPostTemplates, err := rendering.LoadPageTemplate(
+		templateDir,
+		"blog-post.html",
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &BlogHandler{
+		repository:        repo,
+		blogTemplates:     blogTemplates,
+		blogPostTemplates: blogPostTemplates,
+	}, nil
+}
